027048def2d3a7fdf0b7a9dc65ad28270f9bb34c
UnverOnal <onalunver@gmail.com>
2024-08-12 15:56:47 +0300

    Update Input Service.

--- Assets/Scripts/Core/Services/InputService/IInputService.cs
+++ Assets/Scripts/Core/Services/InputService/IInputService.cs
@@ -5,15 +5,17 @@ namespace Services.InputService
 {
     public interface IInputService
     {
-        public event Action<Vector3> OnTap; 
-        public event Action OnRelease; 
-        
+        public event Action<Vector3> OnTap;
+        public event Action<Vector3> OnHold;
+        public event Action OnRelease;
+        public event Action<GameObject> OnItemPicked;
+
         public Vector3 PointerPosition { get;}
-        
+
         public bool IsPointerDown { get;}
         public bool IsPointerUp { get; }
         public bool IsPointerHold { get;}
-        
+
         public void Ignore(bool ignore);
         public int GetSwipe(float sensitivity);
         public Vector2 GetDragInput(float sensitivity);
--- Assets/Scripts/Core/Services/InputService/InputService.cs
+++ Assets/Scripts/Core/Services/InputService/InputService.cs
@@ -6,10 +6,12 @@ namespace Services.InputService
     public class InputService : IInputService
     {
         public event Action<Vector3> OnTap;
+        public event Action<Vector3> OnHold;
         public event Action OnRelease;
-        
+        public event Action<GameObject> OnItemPicked;
+
         public Vector3 PointerPosition { get; private set; }
-        
+
         public bool IsPointerDown => Input.GetMouseButtonDown(0);
         public bool IsPointerUp => Input.GetMouseButtonUp(0);
         public bool IsPointerHold => Input.GetMouseButton(0);
@@ -17,24 +19,28 @@ namespace Services.InputService
         private Vector3 DeltaPosition => PointerPosition - _pointerPreviousPosition;
         private Vector3 _pointerPreviousPosition;
         private Vector3 _startingPosition;
-        
+
         private bool IgnoreInput { get; set; }
-        
+
         public void Update()
         {
             if (IgnoreInput)
                 return;
-            
+
             Track();
 
             if (IsPointerDown)
             {
                 OnTap?.Invoke(PointerPosition);
+                Pick();
                 _startingPosition = PointerPosition;
             }
-            
+
             if (IsPointerUp)
                 OnRelease?.Invoke();
+
+            if (IsPointerHold)
+                OnHold?.Invoke(PointerPosition);
         }
 
         public void Ignore(bool ignore)
@@ -46,13 +52,13 @@ namespace Services.InputService
         {
             var startingPointerPositionWorld = new Vector3(_startingPosition.x, _startingPosition.y, 10f);
             startingPointerPositionWorld = Camera.main.ScreenToWorldPoint(startingPointerPositionWorld);
-                
+
             var pointerPositionWorld = new Vector3(PointerPosition.x, PointerPosition.y, 10f);
             pointerPositionWorld = Camera.main.ScreenToWorldPoint(pointerPositionWorld);
-                
+
             return (pointerPositionWorld - startingPointerPositionWorld).normalized;
         }
-        
+
         public int GetSwipe(float sensitivity)
         {
             var input = Vector3.Dot(Vector3.right, DeltaPosition) * sensitivity / Screen.width;
@@ -73,12 +79,6 @@ namespace Services.InputService
             return dragInput;
         }
 
-        private float GetDragOnSingleAxis(Vector3 axis, float sensitivity)
-        {
-            var input = Vector3.Dot(axis, DeltaPosition) * sensitivity / Screen.width;
-            return input;
-        }
-
         private void Track()
         {
             if (IsPointerDown)
@@ -87,10 +87,52 @@ namespace Services.InputService
                 _pointerPreviousPosition = PointerPosition;
                 return;
             }
-            
+
             _pointerPreviousPosition = PointerPosition;
             if (IsPointerHold)
                 PointerPosition = Input.mousePosition;
+        }
+
+        private float GetDragOnSingleAxis(Vector3 axis, float sensitivity)
+        {
+            var input = Vector3.Dot(axis, DeltaPosition) * sensitivity / Screen.width;
+            return input;
+        }
+
+        ///Picks item selected by using tap/click. Doesn't run if OnItemPicked event is not subscribed.
+        private void Pick()
+        {
+            if(OnItemPicked == null)
+                return;
+
+            var pickedGameObject = GetSelectedGameObject2D();
+            pickedGameObject ??= GetSelectedGameObject3D();
+            if(pickedGameObject != null)
+                OnItemPicked.Invoke(pickedGameObject);
+        }
+
+        private GameObject GetSelectedGameObject3D()
+        {
+            GameObject pickedGameObject = null;
+            var ray = Camera.main.ScreenPointToRay(PointerPosition);
+
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity))
+                pickedGameObject = hit.transform.gameObject;
+
+            return pickedGameObject;
+        }
+
+        private GameObject GetSelectedGameObject2D()
+        {
+            GameObject pickedGameObject = null;
+            Vector2 worldPointerPosition = Camera.main.ScreenToWorldPoint(PointerPosition);
+
+            var hit = Physics2D.Raycast(worldPointerPosition, Vector2.zero);
+
+            if (hit.collider != null)
+                pickedGameObject = hit.collider.gameObject;
+
+            return pickedGameObject;
         }
     }
 }
